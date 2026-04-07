6935e890059a8a8f5ee2d09983e1e4347a84c5c4
Ashish D <25je0892@iitism.ac.in>
2026-04-07 13:40:40 +0000

    updates

--- agent/agent.py
+++ agent/agent.py
@@ -1,11 +1,26 @@
 from langgraph.graph import StateGraph,END,START
 from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
 import langchain
 import langgraph
 from typing import TypedDict,Annotated
 from langchain_core.messages import SystemMessage,AnyMessage,BaseMessage
+from tools import tools
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 class AgentState(TypedDict):
     message : Annotated[BaseMessage, add_messages]
     directory : str
+
+model = ChatOpenAI(
+    model = os.getenv("model" , ""),
+    base_url = os.getenv("model_base_url",""),
+    api_key = os.getenv("model_api_key","")
+)
+
+graph = StateGraph(AgentState)
+
     
