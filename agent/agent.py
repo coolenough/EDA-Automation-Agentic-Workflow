243f957163386d523c65d7e29b298d83300e2ca9
Ashish D <25je0892@iitism.ac.in>
2026-04-07 07:35:22 +0000

    update : AgentState

--- agent/agent.py
+++ agent/agent.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph,END,START
+from langgraph.graph.message import add_messages
 import langchain
 import langgraph
-from typing import TypedDict
+from typing import TypedDict,Annotated
 from langchain_core.messages import SystemMessage,AnyMessage,BaseMessage
 
 class AgentState(TypedDict):
-    message : AnyMessage
+    message : Annotated[BaseMessage, add_messages]
     directory : str
     
