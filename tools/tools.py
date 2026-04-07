14bec362e3a46e3fc91cfe31f70c4b3a38eaab06
Ashish D <25je0892@iitism.ac.in>
2026-04-07 12:39:45 +0530

    Added few more tools

--- tools/tools.py
+++ tools/tools.py
@@ -1,9 +1,25 @@
 import langchain_core.tools import tool
 import pandas as pd
 import numpy as np
+import os
 
 @tool
 def info(df : pd.DataFrame):
     ''' Gives the basic information about the given dataframe such as
-    type of data , column info, size and shape of the dataframe '''
+    type of data , column info, size and shape of the dataframe as a pandas dataframe'''
     return df.info()
+@tool
+def describe(df : pd.DataFrame):
+    '''
+    Gives the description of numerical types of data as pandas dataframe
+    '''
+    return df.describe()
+
+@tool
+def walkthrough_directory():
+    '''
+    Gives the walkthrough to current directory
+    '''
+    return os.walk()
+
+tools = [info,describe,walkthrough_directory]
