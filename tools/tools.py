3391aeebb996daea9ad852327dde13115c68b827
Ashish D <25je0892@iitism.ac.in>
2026-04-22 17:57:38 +0000

    Added correlation heatmap plots

--- tools/tools.py
+++ tools/tools.py
@@ -213,5 +213,25 @@ def getFileName():
 
     return {"filename" : filename , "relative_path" : rel_path}
 
+@tool
+def plot_correlation_heatmap(filename : str , columns : list):
+    '''
+    Plots the correlation heatmap for the given columns
+    '''
 
-agent_tools = [info,describe,walkthrough_directory,install_packages,box_plots,kde_plots,find_unique_values,type_of_categotical_data,piecharts,findnullvalues,findnoofnullvalues,getFileName]
+    df = pd.read_csv(filename)
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(df[columns].corr(), annot=True, cmap='coolwarm', linewidths=0.5)
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format="png")
+    plt.close()
+    
+    image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    buffer.close()
+
+    return image
+
+
+agent_tools = [info,describe,walkthrough_directory,install_packages,box_plots,kde_plots,find_unique_values,type_of_categotical_data,piecharts,findnullvalues,findnoofnullvalues,getFileName,plot_correlation_heatmap]
